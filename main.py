999bbf6bf94ec2034fdd96464999a24bdf495136
Ajai <ajaischandi@gmail.com>
2025-03-15 18:52:31 -0400

    Formatting change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 file_name = "data.csv"
 
-columns = ['Player_ID', 'WL', 'MIN', 'POINTS', 'REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS', 
+columns = ['Player_ID', 'PLAYER_NAME', 'WL', 'MIN', 'POINTS', 'REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS', 
            'PLUS_MINUS', 'TOV', 'FGM', 'FG_PCT', 'FG3M','FG3_PCT', 'FTM', 'FT_PCT', 'OPP TEAM', 'COURT', 'BTB']
 
 with open(file_name, "w", newline="") as f:
@@ -16,15 +16,16 @@ with open(file_name, "w", newline="") as f:
 active_players = players.get_active_players()
 player_ids = [player["id"] for player in active_players]
 
-for player_id in player_ids[:1]:
+for player_id in player_ids[:4]:
+    player_name = players.find_player_by_id(player_id=player_id)['full_name']
     time.sleep(1)
     season = "2023-24"
 
     game_log = PlayerGameLog(player_id=player_id, season=season)
     df = game_log.get_data_frames()[0]
 
-    df = df[['Player_ID', 'WL', 'MIN', 'POINTS', 'REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS', 
-           'PLUS_MINUS', 'TOV', 'FGM', 'FG_PCT', 'FG3M','FG3_PCT', 'FTM', 'FT_PCT', 'OPP TEAM', 'COURT', 'BTB']]
+    df = df[['Player_ID', 'WL', 'MIN', 'PTS', 'REB', 'AST', 'STL', 'BLK', 
+           'PLUS_MINUS', 'TOV', 'FGM', 'FG_PCT', 'FG3M','FG3_PCT', 'FTM', 'FT_PCT', 'MATCHUP', 'GAME_DATE']]
     
     df['COURT'] = df['MATCHUP'].apply(lambda x: 'Home' if 'vs.' in x else 'Away')
     df.loc[df['COURT'] == "Home", 'MATCHUP'] = df['MATCHUP'].str.split('vs.').str[1].str.strip()
@@ -33,6 +34,7 @@ for player_id in player_ids[:1]:
     df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE'], format='%b %d, %Y')
     df['NEXT_GAME_DATE'] = df['GAME_DATE'].shift(-1)
     df['BTB'] = (df['GAME_DATE'] - df['NEXT_GAME_DATE']).dt.days == 1
+    df['PLAYER_NAME'] = player_name
 
     df.rename(columns={
         'PTS': 'POINTS', 
@@ -41,9 +43,8 @@ for player_id in player_ids[:1]:
         'MATCHUP': 'OPP TEAM', 
         'STL': 'STEALS', 
         'BLK': 'BLOCKS',
-        'BTB': 'BACK TO BACK',
     }, inplace=True)
 
     df[columns].to_csv(file_name, mode="a", header=False, index=False)
 
-    print("PLAYER_ID: " + str(player_id) + " DONE")
+    print("PLAYER " + str(player_name) + " DONE")
