31cef3f60ec8546f49e0727b7e242f5d3ef1713f
Ajai <ajaischandi@gmail.com>
2025-03-15 13:26:55 -0400

    Started getting data

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+import csv
+from nba_api.stats.endpoints import PlayerGameLog
 
-# Nikola Jokić
-career = playercareerstats.PlayerCareerStats(player_id='203999') 
+player_id = 201939  # Stephen Curry
+season = "2022-23"
 
-# pandas data frames (optional: pip install pandas)
-career.get_data_frames()[0]
+game_log = PlayerGameLog(player_id=player_id, season=season)
+df = game_log.get_data_frames()[0]
+#df.to_csv("data.csv", index=False)
 
-# json
-career.get_json()
+df = df[['GAME_DATE', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'PLUS_MINUS', 'TOV', 'FGM', 'FG3M', 'MATCHUP', 'MIN']]
 
-# dictionary
-print(career.get_dict())
+df['COURT'] = df['MATCHUP'].apply(lambda x: 'Home' if 'vs.' in x else 'Away')
+
+df.loc[df['COURT'] == "Home", 'MATCHUP'] = df['MATCHUP'].str.split('vs.').str[1].str.strip()
+df.loc[df['COURT'] != "Home", 'MATCHUP'] = df['MATCHUP'].str.split('@').str[1].str.strip()
+
+
+df.rename(columns={'PTS': 'POINTS', 'REB': 'REBOUNDS', 'AST': 'ASSISTS', 'MATCHUP': 'OPP TEAM', 'STL' : 'STEALS', 'BLK': 'BLOCKS'}, inplace=True)
+
+df[['POINTS', 'REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS','PLUS_MINUS','TOV','FGM','FG3M', 'OPP TEAM', 'COURT']].to_csv("data.csv", index=False)
+
+print("CSV file written successfully!")
